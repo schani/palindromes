b4b33a2d84df6c0467053e9860729de9144a0e37
David Siegel <djsiegel@gmail.com>
2013-04-22 18:58:21 -0400

    Discard extra palindrome implementations

--- Palindromes/Palindromes.fs
+++ Palindromes/Palindromes.fs
@@ -1,40 +1,21 @@
 module Palindromes
 
-let width n =
-    if n = 0L then 1
-    else 1 + (double n |> log10 |> int)
-
-let palindrome_string n =
-    let s = n.ToString().ToCharArray()
-    s = Array.rev s
-
-let palindrome_func n =
-    let digits n =
-        let next (width, n) =
-            if width = 0 then None
-            else Some (n % 10L, (width - 1, n / 10L))
-        Seq.unfold next (width n, n)
-
-    let ds = digits n |> Seq.toList
-    ds = List.rev ds
-
-let reverse i =
-    let mutable num = i
-    let mutable reversed = 0L
-    while num <> 0L do
-        let dig  = num % 10L
-        num <- num / 10L
-        reversed <- reversed * 10L + dig
-    reversed
-
-let palindrome_arith n =
+let is_palindrome n =
+    let reverse i =
+        let mutable num = i
+        let mutable reversed = 0L
+        while num <> 0L do
+            let dig  = num % 10L
+            num <- num / 10L
+            reversed <- reversed * 10L + dig
+        reversed
     n = reverse n
 
-let palindromes_between (lower: int64) (upper: int64) is_palindrome =
+let palindromes_between (lower: int64) (upper: int64) =
     seq { lower .. upper }
     |> Seq.filter is_palindrome
 
-let palindromes_squares_between (lower: int64) (upper: int64) is_palindrome =
+let palindromes_squares_between (lower: int64) (upper: int64) =
     let sq = double >> sqrt >> int64
-    palindromes_between (sq lower) (sq upper) is_palindrome
+    palindromes_between (sq lower) (sq upper)
     |> Seq.filter (fun n -> pown n 2 |> is_palindrome)
--- Palindromes/Program.fs
+++ Palindromes/Program.fs
@@ -11,6 +11,6 @@ let main args =
     for case in File.ReadLines ("../../../tests.txt") |> Seq.skip 1 do
         let [upper; lower] = words case |> map int64
         printf "%A %A " upper lower
-        let count = palindromes_squares_between upper lower palindrome_arith |> Seq.length
+        let count = palindromes_squares_between upper lower |> Seq.length
         printfn "%d" count
     0
