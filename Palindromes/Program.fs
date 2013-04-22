6397a66a02ff83accc5962e18f02ae546607529a
David Siegel <djsiegel@gmail.com>
2013-04-22 19:01:05 -0400

    Simplify project

--- Palindromes/Program.fs
+++ Palindromes/Program.fs
@@ -3,13 +3,30 @@ module Main
 open System
 open System.IO
 
-open Util
-open Palindromes
+let is_palindrome n =
+    let reverse i =
+        let mutable num = i
+        let mutable reversed = 0L
+        while num <> 0L do
+            let dig  = num % 10L
+            num <- num / 10L
+            reversed <- reversed * 10L + dig
+        reversed
+    n = reverse n
+
+let palindromes_between (lower: int64) (upper: int64) =
+    seq { lower .. upper }
+    |> Seq.filter is_palindrome
+
+let palindromes_squares_between (lower: int64) (upper: int64) =
+    let sq = double >> sqrt >> int64
+    palindromes_between (sq lower) (sq upper)
+    |> Seq.filter (fun n -> pown n 2 |> is_palindrome)
 
 [<EntryPoint>]
 let main args =
     for case in File.ReadLines ("../../../tests.txt") |> Seq.skip 1 do
-        let [upper; lower] = words case |> map int64
+        let [|upper; lower|] = case.Split([|' '|]) |> Array.map int64
         printf "%A %A " upper lower
         let count = palindromes_squares_between upper lower |> Seq.length
         printfn "%d" count
