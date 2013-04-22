5a2d74e246dce6a9dffeae13943f84b54cb88227
David Siegel <djsiegel@gmail.com>
2013-04-22 18:54:45 -0400

    Try iterating without seqs

--- Palindromes/Program.fs
+++ Palindromes/Program.fs
@@ -11,6 +11,6 @@ let main args =
     for case in File.ReadLines ("../../../tests.txt") |> Seq.skip 1 do
         let [upper; lower] = words case |> map int64
         printf "%A %A " upper lower
-        let count = palindromes_squares_between upper lower palindrome_arith |> Seq.length
+        let count = palindromes_squares_between_noseq upper lower palindrome_arith |> List.length
         printfn "%d" count
     0
