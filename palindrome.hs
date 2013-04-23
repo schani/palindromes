218f31ff21ee33dc9207463f9e0b763cf67830fb
David Siegel <djsiegel@gmail.com>
2013-04-23 19:12:50 -0400

    Cleaner

--- palindrome.hs
+++ palindrome.hs
@@ -3,20 +3,20 @@ module Palindrome where
 import Data.Int
 import Control.Monad
 
-is_palindrome :: Int64 -> Bool
 is_palindrome n = n == go n 0
   where go 0 rev = rev
         go acc rev = go (div acc 10) (rev * 10 + mod acc 10)
 
 palindromes_between low high = filter is_palindrome [low..high]
 
-palindromes_squares_between low high = filter square_is_palindrome $ palindromes_between low' high'
+palindromes_squares_between low high = filter squares $ palindromes_between low' high'
   where sq = sqrt . fromIntegral
         low' = ceiling (sq low)
         high' = floor (sq high)
-        square_is_palindrome x = is_palindrome (x ^ 2)
+        squares x = is_palindrome (x ^ 2)
 
-run = length . (\[x, y] -> palindromes_squares_between x y) . map read . words
+run test = length $ palindromes_squares_between low high
+	where [low, high] = map read (words test) :: [Int64]
 
 main = do
   count : tests <- fmap lines (readFile "tests.txt")
